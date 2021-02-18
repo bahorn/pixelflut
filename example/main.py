8d989fff87ad89d2567d337ee3e78f938819a953
B Horn <b@horn.uk>
2021-02-18 02:09:20 +0100

    Adding more documentation to the examples.
    Signed-off-by: B Horn <b@horn.uk>

--- example/main.py
+++ example/main.py
@@ -2,14 +2,19 @@ import random
 import time
 import sys
 import math
-import struct
 from PIL import Image
 
 from pixelflut import random_color, rgba, PixelFlut
 
 class Examples:
+    """
+    Our example codes.
+    """
     @staticmethod
     def random_walk(pf):
+        """
+        Random walk, basically ends up looking like a cool fractal.
+        """
         (width, height) = pf.dims()
         x = width/2
         y = height/2
@@ -25,6 +30,9 @@ class Examples:
 
     @staticmethod
     def grid(pf):
+        """
+        This draws a general grid.
+        """
         (width, height) = pf.dims()
         for k in range(0, width, 100):
             for v in range(0, width):
@@ -33,6 +41,9 @@ class Examples:
 
     @staticmethod
     def clear(pf):
+        """
+        Sets the screen to a single color.
+        """
         (width, height) = pf.dims()
         while True:
             for x in range(width):
@@ -41,6 +52,9 @@ class Examples:
 
     @staticmethod
     def spiral(pf):
+        """
+        Draws a spiral
+        """
         (width, height) = pf.dims()
         center_x = width/2
         center_y = height/2
@@ -51,42 +65,64 @@ class Examples:
             t %= maxr
             x = abs(center_x + 2*math.floor(10*t*math.sin(t)))
             y = abs(center_y + 2*math.floor(10*t*math.cos(t)))
-            pf.set(x, y,
-                   rgba(255-math.floor(128*(t/maxr))
-                        , math.floor(255*(t/maxr))
-                        , 100-math.floor(64*(t/maxr)), 10))
+            color = rgba(
+                255-math.floor(128*(t/maxr)),
+                math.floor(255*(t/maxr)),
+                100-math.floor(64*(t/maxr)),
+                10
+            )
+            pf.set(x, y, color)
 
     @staticmethod
     def image(pf):
-        (width, height) = pf.dims()
-        
-        img = Image.open('covhack.png')
+        """
+        Draws an image and makes it loop around the screen.
+        """
+        # Some generic default settings.
+        t = 0
         rsz_x = 128
         rsz_y = 128
+        direction_x = 1
+        direction_y = 1
+        speed = 128
+
+        (width, height) = pf.dims()
+
+        # Pick a random location to draw
+        offset_x = random.randint(0, width % rsz_x) * rsz_x
+        offset_y = random.randint(0, height % rsz_y) * rsz_y
+        
+        # Load, resize and make it a binary image.
+        img = Image.open('covhack.png')
         rsz = img.resize((rsz_x, rsz_y))
         out = rsz.convert('1')
-        offset_x = random.randint(0, width)
-        offset_y = random.randint(0, height)
-        t = 0
+        
         while True:
-            t += 0.1
+            t += 0.01
+            # Chose a random colour
             r = random.randint(0, 255)
             g = random.randint(0, 255)
             b = random.randint(0, 255)
-            offset_x += 50 #random.randint(0,width)
-            offset_y += 50 #random.randint(0,height)
+
+            offset_x += direction_x*speed
+            offset_y += direction_y*speed
+            
+            # Loop through every pixel and print it.
             for x in range(rsz_x):
                 for y in range(rsz_y):
+                    # Skip any pixel with an grayscale value of 0.
                     a = out.getpixel((x,y))
+                    
                     if a == 0:
                         continue
+           
                     color = rgba(r, g, b, int(a))
+           
                     pf.set(
-                        int(offset_x+1*x) % width, 
-                        int(offset_y+1*y) % height, 
+                        int(offset_x+x) % width, 
+                        int(offset_y+y) % height, 
                         color
                     )
-
 samples = {
     'image': Examples.image,
     'grid': Examples.grid,
@@ -96,11 +132,13 @@ samples = {
 }
 
 if __name__ == '__main__':
+    #pf = PixelFlut(('localhost', 1337))
     pf = PixelFlut(('entry.athon.uk', 1337))
 
     if len(sys.argv) < 2:
         exit()
     
+    # Call a function in `samples` by the name we provide.
     samples.get(
         sys.argv[1],
         lambda _: print('unknown function')
--- example/pixelflut.py
+++ example/pixelflut.py
@@ -7,36 +7,68 @@ import math
 import struct
 
 class PixelFlut:
+    """
+    A quick and easy pixel flut client.
+    """
     def __init__(self, remote):
+        """
+        Connects to the server.
+        """
         self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.s.connect(remote)
 
     def set(self, x, y, value):
+        """
+        Change a pixel on the display.
+        """
         self.s.send(
             bytes('PX {:g} {:g} {:s}\n'.format(x, y, value), 'ascii')
         )
 
     def get(self, x, y):
+        """
+        Gets the value of a pixel on the display.
+        """
         self.s.send(bytes('PX {} {}\n'.format(x, y), 'ascii'))
         res = self.s.recv(1024)
         hexc = res[:-1].decode('ascii').split(' ')[-1]
         return tuple(bytes.fromhex(hexc))
     
     def dims(self):
+        """
+        Returns the dimensions of the screen.
+        """
         self.s.send(b'SIZE\n')
         res = self.s.recv(1024).decode('ascii')
         return list(map(int, res[:-1].split(' ')[1:]))
 
+    def clear(self):
+        """
+        A custom command, you probably don't need this.
+        """
+        self.s.send(b'CLEAR\n')
 
-def clamp(x): 
-  return max(0, min(x, 255))
+
+def clamp(x):
+    """
+    Limits the values into the range 0-255
+    """
+    return max(0, min(x, 255))
 
 def rgba(r,g,b,a):
-    return binascii.hexlify(
-        struct.pack('BBBB', clamp(r), clamp(g), clamp(b),
-                    clamp(a))).decode('ascii')
+    """
+    Packs an RGBA value in a form thats suitable for pixelflut.
+    """
+    color = struct.pack(
+        'BBBB',
+        clamp(r), clamp(g), clamp(b), clamp(a)
+    )
+    return binascii.hexlify(color).decode('ascii')
 
 def random_color(alpha):
+    """
+    Returns a random color.
+    """
     return rgba(
         random.randint(0, 255),
         random.randint(0, 255),
